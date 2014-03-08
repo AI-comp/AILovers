d1a46cfddb3d105e2600e63185b945eeba89e8a3
Frederik Nakstad <nakstad@gmail.com>
2014-03-08 14:00:48 +0900

    Create structure for custom JS, LESS/CSS, images, and integrate into development workflow.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,37 @@ module.exports = function(grunt) {
 
     // Load tasks
     grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
 
     grunt.initConfig({
+
+        less: {
+            development: {
+                options: {
+                    paths: ["assets/css"]
+                },
+                files: {
+                    "client/css/app.css": "client/css/app.less"
+                }
+            }
+        },
+
+        watch: {
+            styles: {
+                // Which files to watch (all .less files recursively in the less directory)
+                files: ['client/css/**/*.less'],
+                    tasks: ['less'],
+                    options: {
+                        nospawn: true
+                    }
+            }
+        },
+
+        concurrent: {
+            dev: ['nodemon:dev', 'watch']
+        },
 
         nodemon: {
             dev: {
@@ -15,6 +44,6 @@ module.exports = function(grunt) {
 
     });
 
-    grunt.registerTask('dev', ['nodemon']);
+    grunt.registerTask('dev', ['concurrent:dev']);
 
 };
---
+++ client/js/app.js
@@ -0,0 +1,1 @@
+console.log('Tokimeki Memorial');
