9578954c6b1817ff7a8e8bfa0a4532835c8040e9
Frederik Nakstad <nakstad@gmail.com>
2014-04-01 12:06:51 +0900

    Make WS connection in index page for testing purposes.

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,9 @@
 console.log('Tokimeki Memorial');
+
+var host = window.document.location.host.replace(/:.*/, '');
+var ws = new WebSocket('ws://' + host + ':8000');
+
+ws.onmessage = function (event) {
+    //console.log(event);
+    //console.log(JSON.parse(event.data));
+};
