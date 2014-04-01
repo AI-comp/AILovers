739cf194162e66afd162bc455a0e2b488ef13b6f
Frederik Nakstad <nakstad@gmail.com>
2014-04-01 12:31:08 +0900

    Connect AI runner to WebSockets server

--- client/js/app.js
+++ client/js/app.js
@@ -2,15 +2,20 @@ console.log('Tokimeki Memorial');
 
 $(function() {
     var wsTestBtn = $('#wsTest');
-    var log = $('#log');
 
     wsTestBtn.click(function() {
         var host = window.document.location.host.replace(/:.*/, '');
         var ws = new WebSocket('ws://' + host + ':8000');
 
         ws.onmessage = function (event) {
-            console.log(event);
+            log(JSON.parse(event.data));
             //console.log(JSON.parse(event.data));
         };
     });
+
+    function log(message) {
+        $('#log').append(
+            $('<li>').append(message)
+        );
+    }
 });
--- server/wsEvents.js
+++ server/wsEvents.js
@@ -1,13 +1,17 @@
+var Runner = require('./runner').Runner;
+
 module.exports = function (ws) {
 
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(process.memoryUsage()), function() { /* ignore errors */ });
+    /*var id = setInterval(function() {
+        ws.send(JSON.stringify(process.memoryUsage()), function() { });
     }, 1000);
-    console.log('started client interval');
+    console.log('started client interval');*/
+
+    var runner = new Runner();
+    runner.runGame();
 
     ws.on('close', function() {
-        console.log('stopping client interval');
-        clearInterval(id);
+        console.log('Closing WS connection');
     });
 
 };
