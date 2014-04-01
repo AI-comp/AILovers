7ef0eff53f3c9016acf44e6e2347453e6d6a1949
Frederik Nakstad <nakstad@gmail.com>
2014-04-01 16:42:22 +0900

    Add better event structure for WS server

--- client/js/app.js
+++ client/js/app.js
@@ -2,16 +2,18 @@ console.log('Tokimeki Memorial');
 
 $(function() {
     var wsTestBtn = $('#wsTest');
+    var host = window.document.location.host.replace(/:.*/, '');
+    var ws = new WebSocket('ws://' + host + ':8000');
 
     wsTestBtn.click(function() {
-        var host = window.document.location.host.replace(/:.*/, '');
-        var ws = new WebSocket('ws://' + host + ':8000');
-
-        ws.onmessage = function (event) {
-            log(event.data.replace(/\n/g, '<br />'));
-            //console.log(JSON.parse(event.data));
-        };
+        ws.send({
+            programs: ['program1', 'program2', 'program3']
+        });
     });
+
+    ws.onmessage = function (event) {
+        log(event.data.replace(/\n/g, '<br />'));
+    };
 
     function log(message) {
         $('#log').append(message);
--- server/wsEvents.js
+++ server/wsEvents.js
@@ -7,10 +7,12 @@ module.exports = function (ws) {
     }, 1000);
     console.log('started client interval');*/
 
-    var runner = new Runner();
-    runner.runGame(function() {
-        ws.send(runner.gameResult, function() { /* No error handling yet */ });
-    });
+    ws.onmessage = function(event) {
+        var runner = new Runner();
+        runner.runGame(function() {
+            ws.send(runner.gameResult, function() { /* No error handling yet */ });
+        });
+    }
 
     ws.on('close', function() {
         console.log('Closing WS connection');
