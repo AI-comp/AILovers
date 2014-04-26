d105d1eabb28fa8ab2e6ed5ffadcc4b7ac62ed37
ss0201 <ss0201.777@gmail.com>
2014-04-27 00:09:07 +0900

    Add cocos2d

--- server/routes.js
+++ server/routes.js
@@ -5,9 +5,15 @@ var express = require('express'),
 module.exports = function (app) {
 
     app.use(express.static(path.join(__dirname, '../client')));
-
+    app.use('/cocos2d', express.static(path.join(__dirname, '../client/cocos2d')));
+    app.use('/replay', express.static(path.join(__dirname, '../client/js')));
+    
     app.get('/', function (req, res) {
         res.render('index');
+    });
+
+    app.get('/replay', function (req, res) {
+        res.render('replay');
     });
 
 };
