507ecacd4d961c1ce0422967b0686b59fb7750d7
ss0201 <ss0201.777@gmail.com>
2014-05-01 16:13:42 +0900

    Show heroine faces

--- client/replayer/src/mainScene.js
+++ client/replayer/src/mainScene.js
@@ -5,6 +5,7 @@ var MainScene = cc.Scene.extend({
         this.sceneNode = ccs.sceneReader.createNodeWithSceneFile(res.MainScene_json);
         this.addChild(this.sceneNode);
 
+        this.loadHeroineImages(game.heroines.length);
         this.setGameStatus(game);
 
         var controlPanelNode = this.sceneNode.getChildByTag(100);
@@ -18,15 +19,26 @@ var MainScene = cc.Scene.extend({
         return true;
     },
 
+    loadHeroineImages: function (numHeroines) {
+        for (var heroineIndex = 0; heroineIndex < numHeroines; heroineIndex++) {
+            var heroinePanel = this.getHeroinePanel(heroineIndex);
+            heroinePanel.getChildByName('FaceImage').loadTexture('res/face/' + heroineIndex + '.png');
+        }
+    },
+
     setGameStatus: function (game) {
         for (var heroineIndex = 0; heroineIndex < game.heroines.length; heroineIndex++) {
             var heroine = game.heroines[heroineIndex];
-            var heroinePanelNode = this.sceneNode.getChildByTag(heroineIndex);
-            var heroinePanel = heroinePanelNode.getChildByTag(0);
+            var heroinePanel = this.getHeroinePanel(heroineIndex);
             heroinePanel.getChildByName('EnthusiasmLabel').setText(heroine.enthusiasm);
             for (var playerIndex = 0; playerIndex < game.numPlayers; playerIndex++) {
                 heroinePanel.getChildByName('LoveLabel' + playerIndex).setText(heroine.revealedLove[playerIndex]);
             }
         }
+    },
+
+    getHeroinePanel: function (heroineIndex) {
+        var heroinePanelNode = this.sceneNode.getChildByTag(heroineIndex);
+        return heroinePanelNode.getChildByTag(0);
     },
 });
