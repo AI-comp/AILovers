7a435706dcb15a5ed7eae98db7039c665e4a1fcb
ss0201 <ss0201.777@gmail.com>
2014-05-01 15:24:14 +0900

    Add some image files

--- client/replayer/src/resource.js
+++ client/replayer/src/resource.js
@@ -1,8 +1,4 @@
 var res = {
-    HelloWorld_png: "res/HelloWorld.png",
-    CloseNormal_png: "res/CloseNormal.png",
-    CloseSelected_png: "res/CloseSelected.png",
-
     HeroinePanel_json: "res/publish/HeroinePanel.json",
     DatePanel_json: "res/publish/DatePanel.json",
     ControlPanel_json: "res/publish/ControlPanel.json",
@@ -11,10 +7,6 @@ var res = {
 };
 
 var g_resources = [
-    res.HelloWorld_png,
-    res.CloseNormal_png,
-    res.CloseSelected_png,
-
     res.HeroinePanel_json,
     res.DatePanel_json,
     res.ControlPanel_json,
