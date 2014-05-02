485bc41789b5c3a681435a6efefb39701cd2f4b3
ss0201 <ss0201.777@gmail.com>
2014-05-02 18:05:27 +0900

    Change execution order

--- client/replayer/main.js
+++ client/replayer/main.js
@@ -2,19 +2,19 @@ cc.game.onStart = function () {
     cc.view.setDesignResolutionSize(1024, 768, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(true);
 
-    cc.LoaderScene.preload(g_resources, function () {
-        var seed = typeof replay !== 'undefined' ? replay.seed : 0;
-        var game = new Game(seed);
-        game.initialize(4);
-        var defaultCommands = _.map(_.range(10), function (i) {
-            return _.map(_.range(4), function (j) {
-                return _.range(5);
-            });
+    var seed = typeof replay !== 'undefined' ? replay.seed : 0;
+    var game = new Game(seed);
+    game.initialize(4);
+    var defaultCommands = _.map(_.range(10), function (i) {
+        return _.map(_.range(4), function (j) {
+            return _.range(5);
         });
-        var commands = typeof replay !== 'undefined' ? replay.commands : defaultCommands;
-        ReplayerScene.prototype.game = game;
-        ReplayerScene.prototype.commands = commands;
+    });
+    var commands = typeof replay !== 'undefined' ? replay.commands : defaultCommands;
+    ReplayerScene.prototype.game = game;
+    ReplayerScene.prototype.commands = commands;
 
+    cc.LoaderScene.preload(g_resources, function () {
         cc.director.runScene(new MainScene());
     }, this);
 };
