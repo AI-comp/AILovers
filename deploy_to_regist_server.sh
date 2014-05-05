1106ea55c48ae29fc8c1d7af5ba351c821a83a55
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-05-05 03:58:59 +0000

    Fix deploy script.

--- deploy_to_regist_server.sh
+++ deploy_to_regist_server.sh
@@ -1,2 +1,14 @@
+#!/bin/sh
+ORIGINAL=$(pwd)
+CURRENT=$(cd $(dirname $0) && pwd)
+
+cd $CURRENT
+
 npm install
 bower install
+
+mkdir -p ../../public/contests/
+cp -R client ../../public/contests/1
+mv ../../public/contests/1/replayer ../../public/contests/1/results
+
+cd $ORIGINAL
