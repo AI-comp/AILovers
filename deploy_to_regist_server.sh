c771f83adb1e258c8a531c0e23bdb100ddbae0e5
ss0201 <ss0201.777@gmail.com>
2014-07-02 18:40:46 +0900

    Update deploy_to_regist_server.sh

--- deploy_to_regist_server.sh
+++ deploy_to_regist_server.sh
@@ -11,6 +11,6 @@ grunt setup
 
 rm -Rf ../../public/contests/1
 cp -R client ../../public/contests/1
-mv ../../public/contests/1/replayer ../../public/contests/1/results
+mv ../../public/contests/1/replayer ../../public/contests/1/battle_results
 
 cd $ORIGINAL
