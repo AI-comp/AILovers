37a194f9d32014b9b79f3e49371804d026ea79c6
hugh3166 <hugh3166@gmail.com>
2014-07-13 10:38:07 +0900

    Animate Score bar

--- client/replayer/src/resultScene.js
+++ client/replayer/src/resultScene.js
@@ -17,22 +17,55 @@ var ResultScene = InformationScene.extend({
 
         var rankedPlayers = this.game.getRanking();
         var maxPopularity = _.max(_.map(rankedPlayers, function (player) {
+            return Math.abs(player.getPopularity());
+        }, this));
+
+        var minusPopularity = _.min(_.map(rankedPlayers, function (player) {
             return player.getPopularity();
         }, this));
+        minusPopularity = minusPopularity >= 0 ? 0 : minusPopularity
+        var minusPercent = Math.abs(minusPopularity) / maxPopularity * 100;
 
         _.each(rankedPlayers, function (player) {
             var playerPanel = this.getPlayerPanel(player.index);
             var popularityBarName = (player.getPopularity() >= 0 ? 'PositivePopularityBar' : 'NegativePopularityBar');
             var popularityBar = playerPanel.getChildByName(popularityBarName);
             popularityBar.loadTexture(res.image.info.revealedBars[player.index]);
-            popularityBar.setPercent(Math.abs(player.getPopularity()) / maxPopularity * 100);
 
-            if (this.game.getWinner() == player.index) {
+            var percent = Math.abs(player.getPopularity()) / maxPopularity * 100;
+            popularityBar.setPercent(0);
+            // popularityBar.setPercent(percent % ResultScene.INTERVAL_PERCENT);
+
+            popularityBar.setTag(percent);
+            // popularityBar.prototype.isWinner = this.game.getWinner() == player.index;
+
+            var repeatCount = percent / ResultScene.INTERVAL_PERCENT;
+            var delay = player.getPopularity() <= 0 ? 0 : minusPercent / ResultScene.INTERVAL_PERCENT * ResultScene.INTERVAL * 2
+            popularityBar.schedule(this.intervalPercent, ResultScene.INTERVAL, repeatCount, delay);
+
+            if (this.game.getWinner() === player.index) {
                 var winImage = playerPanel.getChildByName("WinnerImage");
-                winImage.setVisible(true);
-                var pulseSequence = new cc.Sequence(new Array(new cc.FadeTo(0.6, 100), new cc.FadeOut(0.6, 255)));
-                winImage.runAction(new cc.RepeatForever(pulseSequence));
+                winImage.scheduleOnce(this.delayShowWinner, 
+                    repeatCount * ResultScene.INTERVAL * 2 + delay);
             }
         }, this);
     },
+
+    intervalPercent: function(delta) {
+        // cc.log(delta);
+        var newPercent = this.getPercent() + ResultScene.INTERVAL_PERCENT;
+        newPercent = (newPercent <= this.getTag()) ? newPercent : this.getTag();
+        this.setPercent(newPercent);
+    },
+
+    delayShowWinner: function() {
+        this.setVisible(true);
+        var pulseSequence = new cc.Sequence(new Array(new cc.FadeTo(ResultScene.INTERVAL_PULSE, 100),
+            new cc.FadeOut(ResultScene.INTERVAL_PULSE, 255)));
+        this.runAction(new cc.RepeatForever(pulseSequence));
+    },
 });
+
+ResultScene.INTERVAL = 0.01
+ResultScene.INTERVAL_PERCENT = 1
+ResultScene.INTERVAL_PULSE = 0.6
